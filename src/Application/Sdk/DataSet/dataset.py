1db6377f21d46dc6c54fbaee8f5bdd5e63b1fda5
gaoqf <1435208021@qq.com>
2022-10-10 21:38:46 +0800

    upgrade:添加DataSet CPython实现

--- src/Application/Sdk/DataSet/dataset.py
+++ src/Application/Sdk/DataSet/dataset.py
@@ -2,21 +2,38 @@ import ctypes
 import os
 import time
 
+
+def Callback(action, method, param):
+    return method
+
+
+def ReleaseCallbackReturn(returnValue):
+    return
+
+
 path = os.path.abspath('./src/Application/Sdk/cosmos.communication.dll')
-QueryFunc = ctypes.CDLL(path).Query
-QueryFunc.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+dll = ctypes.CDLL(path)
 
-StartFunc = ctypes.CDLL(path).Start
+CChar = ctypes.c_char_p
+CPointer = ctypes.c_void_p(None)
+CallbackPointer = (ctypes.CFUNCTYPE(
+    CChar, CChar, CChar, CChar))(Callback)
+ReleaseCallbackReturnPointer = (ctypes.CFUNCTYPE(
+    None, CChar))(ReleaseCallbackReturn)
 
-PushFunc = ctypes.CDLL(path).Push
+InitFunc = dll.Init
+InitFunc.argtypes = [CPointer, CPointer, CChar, CChar]
 
-value = "test"
+PushFunc = dll.Push
+PushFunc.argtypes = [CChar]
 
 if __name__ == '__main__':
-    StartFunc()
-    QueryFunc(value.encode("utf-8"), value.encode("utf-8"))
+    processorAddr = "0.0.0.0:8068"
+    vendorAddr = "0.0.0.0:9068"
+    InitFunc(CallbackPointer, ReleaseCallbackReturnPointer, processorAddr.encode(
+        "utf-8"), vendorAddr.encode("utf-8"))
 
-    time.sleep(2)
-    PushFunc()
-    time.sleep(2)
-    PushFunc()
+    while (True):
+        time.sleep(2)
+        pushParam = "{\"Ids\":[\"1\",\"2\"],\"Value\":\"test\"}"
+        PushFunc(pushParam.encode("utf-8"))
